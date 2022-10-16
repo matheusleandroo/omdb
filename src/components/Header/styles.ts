fb849c839f9d917f8e1dcb0bc6c7677fbd89b058
Matheus Leandro <mattleandroo@outlook.com>
2022-10-16 17:02:24 -0300

    changing layout

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,6 +1,13 @@
 import styled from 'styled-components'
 
-export const HeaderContainer = styled.strong`
+export const HeaderContainer = styled.header`
   background-color: ${(props) => props.theme.primary};
   color: ${(props) => props.theme.white};
+  display: flex;
+  justify-content: center;
+  padding: 1.25rem;
+  font-weight: bold;
+  font-size: 2rem;
+  position: fixed;
+  width: 100vw;
 `
