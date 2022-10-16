4e45f6a35566817ef0bb358e1f00b7be56e33dc5
Matheus Leandro <mattleandroo@outlook.com>
2022-10-16 17:03:10 -0300

    creating movie content

---
+++ src/components/MovieContent/index.tsx
@@ -0,0 +1,35 @@
+import Popup from 'reactjs-popup'
+import { IMovies } from '../../interfaces/IMovies'
+import { MovieContainer } from './styles'
+import svgImageNotFound from '../../assets/image-not-found.svg'
+import { NavLink } from 'react-router-dom'
+import 'reactjs-popup/dist/index.css'
+
+interface IMovieContent {
+  movie: IMovies
+}
+
+export function MovieContent({ movie }: IMovieContent) {
+  return (
+    <MovieContainer>
+      <Popup
+        trigger={
+          <img
+            src={movie.Poster !== 'N/A' ? movie.Poster : svgImageNotFound}
+            alt="Poster do filme"
+          />
+        }
+        position="center center"
+      >
+        <div>
+          <p>Title: {movie.Title}</p>
+          <p>Year: {movie.Year}</p>
+          <p>Type: {movie.Type}</p>
+          <p>imdbID: {movie.imdbID}</p>
+
+          <NavLink to={`/details/${movie.imdbID}`}>+ info</NavLink>
+        </div>
+      </Popup>
+    </MovieContainer>
+  )
+}
