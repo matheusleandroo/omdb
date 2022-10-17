7c284b9d4c62e1600bc07f072b57123abe9a92b7
Matheus Leandro <mattleandroo@outlook.com>
2022-10-17 14:14:23 -0300

    add style

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,39 @@
+import styled from 'styled-components'
+
+interface ContainerProps {
+  isErrored: boolean
+}
+
+export const InputContainer = styled.div<ContainerProps>`
+  display: flex;
+  flex-direction: column;
+
+  label,
+  input,
+  span {
+    margin-bottom: 0.375rem;
+  }
+
+  label {
+    font-weight: bold;
+  }
+
+  input {
+    border-radius: 0.75rem;
+    height: 2.375rem;
+    border: 1px solid ${(props) => props.theme['gray-200']};
+
+    padding: 0.5rem;
+
+    &:focus {
+      border-color: ${(props) =>
+        props.isErrored ? props.theme['primary-dark'] : props.theme.black};
+    }
+  }
+  span {
+    font-weight: bold;
+    font-size: 12px;
+    margin-left: 5px;
+    color: ${(props) => props.theme['primary-dark']};
+  }
+`
