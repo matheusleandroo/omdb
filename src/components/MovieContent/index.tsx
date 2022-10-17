342ef6d37d23f77af76997b0bedfd490ea03af3c
Matheus Leandro <mattleandroo@outlook.com>
2022-10-17 14:15:38 -0300

    changing styles

--- src/components/MovieContent/index.tsx
+++ src/components/MovieContent/index.tsx
@@ -1,10 +1,11 @@
 import Popup from 'reactjs-popup'
 import { IMovies } from '../../interfaces/IMovies'
-import { MovieContainer } from './styles'
+import { MovieContainer, MovieInfos } from './styles'
 import svgImageNotFound from '../../assets/image-not-found.svg'
 import { NavLink } from 'react-router-dom'
 import 'reactjs-popup/dist/index.css'
 import { Button } from '../Button'
+import { FiPlus } from 'react-icons/fi'
 
 interface IMovieContent {
   movie: IMovies
@@ -17,21 +18,30 @@ export function MovieContent({ movie }: IMovieContent) {
         trigger={
           <img
             src={movie.Poster !== 'N/A' ? movie.Poster : svgImageNotFound}
-            alt="Poster do filme"
+            alt="Poster"
           />
         }
+        arrow={false}
         position="center center"
       >
-        <div>
-          <p>Title: {movie.Title}</p>
-          <p>Year: {movie.Year}</p>
-          <p>Type: {movie.Type}</p>
-          <p>imdbID: {movie.imdbID}</p>
+        <MovieInfos>
+          <p>
+            <strong>Title</strong>: {movie.Title}
+          </p>
+          <p>
+            <strong>Year</strong>: {movie.Year}
+          </p>
+          <p>
+            <strong>Type</strong>: {movie.Type}
+          </p>
+          <p>
+            <strong>imdbID</strong>: {movie.imdbID}
+          </p>
 
           <NavLink to={`/details/${movie.imdbID}`}>
-            <Button title="+ info" />
+            <Button title="info" icon={FiPlus} />
           </NavLink>
-        </div>
+        </MovieInfos>
       </Popup>
     </MovieContainer>
   )
--- src/components/MovieContent/styles.ts
+++ src/components/MovieContent/styles.ts
@@ -1,5 +1,34 @@
 import styled from 'styled-components'
 
 export const MovieContainer = styled.div`
-  background-color: red;
+  background-color: ${(props) => props.theme['gray-100']};
+  border-radius: 8px;
+  padding: 0.7rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 1.5rem;
+
+  img {
+    height: 350px;
+    max-width: 250px;
+    cursor: pointer;
+  }
+`
+
+export const MovieInfos = styled.div`
+  padding: 1rem 0.5rem;
+
+  p {
+    margin-bottom: 0.5rem;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  button {
+    margin-top: 1.5rem;
+  }
 `
