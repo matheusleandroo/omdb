862d24b2f4231d86b854e622a705ca0ac6f6490d
Matheus Leandro <mattleandroo@outlook.com>
2022-10-17 14:12:45 -0300

    add icon

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { IconBaseProps } from 'react-icons'
 
 import { ButtonContainer } from './styles'
 
 interface IButton extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   title: string
+  icon?: React.ComponentType<IconBaseProps>
 }
 
-export function Button({ title = 'Buscar', ...props }: IButton) {
-  return <ButtonContainer {...props}>{title}</ButtonContainer>
+export function Button({ title = 'Buscar', icon: Icon, ...props }: IButton) {
+  return (
+    <ButtonContainer {...props}>
+      {Icon && <Icon size={20} />}
+      {title}
+    </ButtonContainer>
+  )
 }
--- src/components/Button/styles.tsx
+++ src/components/Button/styles.tsx
@@ -3,15 +3,22 @@ import styled from 'styled-components'
 export const ButtonContainer = styled.button`
   width: 100%;
   border-radius: 0.75rem;
-  height: 3.375rem;
+  height: 2.375rem;
   padding: 1rem 0.75rem;
   background-color: ${(props) => props.theme.primary};
   color: ${(props) => props.theme.white};
   font-weight: bold;
   font-size: 1rem;
   border: 1px solid transparent;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
   transition: background-color 0.2s;
+
+  svg {
+    margin-right: 0.5rem;
+  }
 
   &:hover {
     background: ${(props) => props.theme['primary-dark']};
