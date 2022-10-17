b81e383a7bd7c8e5e29ef71b28ea11f760b3e3ac
Matheus Leandro <mattleandroo@outlook.com>
2022-10-16 21:51:15 -0300

    adding react hook form and input component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,32 +1,42 @@
 import { useNavigate } from 'react-router-dom'
+import { useForm, SubmitHandler, FieldValues } from 'react-hook-form'
 import { Button } from '../../components/Button'
 import { useFilters } from '../../hooks/Filters'
 
 import { HomeContainer } from './styles'
+import { Input } from '../../components/Input'
 
 export function Home() {
   const navigate = useNavigate()
 
   const { filters, updateFilters } = useFilters()
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      title: filters.value,
+    },
+  })
+  const onSubmit: SubmitHandler<FieldValues> = (data) => navigate('/listing')
+
   return (
     <HomeContainer>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault()
-          navigate('/listing')
-        }}
-      >
-        <input
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          id="title"
           type="text"
-          placeholder="Procure seu Filme"
-          value={filters.value}
-          onChange={(value) =>
+          name="title"
+          register={register}
+          onChange={(e) =>
             updateFilters({
               ...filters,
-              value: value.target.value,
+              value: e.target.value,
             })
           }
+          error={errors.title}
         />
         <Button title="Buscar" />
       </form>
