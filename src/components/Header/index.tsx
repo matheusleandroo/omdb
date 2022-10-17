0b73de0d439c1d1bfe86b630b2d4a54c669c174e
Matheus Leandro <mattleandroo@outlook.com>
2022-10-16 21:49:59 -0300

    updating filters on click

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,22 @@
 import { NavLink } from 'react-router-dom'
+import { useFilters } from '../../hooks/Filters'
 
 import { HeaderContainer } from './styles'
 
 export function Header() {
+  const { filters, updateFilters } = useFilters()
   return (
     <HeaderContainer>
-      <NavLink to="/" title="OMDb">
+      <NavLink
+        to="/"
+        title="OMDb"
+        onClick={() =>
+          updateFilters({
+            ...filters,
+            value: '',
+          })
+        }
+      >
         OMDb
       </NavLink>
     </HeaderContainer>
