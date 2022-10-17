281f4e8e90f3d8fe86b3ac180b73a75327ddd29f
Matheus Leandro <mattleandroo@outlook.com>
2022-10-16 21:50:43 -0300

    creating input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import {
+  UseFormRegister,
+  FieldValues,
+  FieldError,
+  Merge,
+  FieldErrorsImpl,
+} from 'react-hook-form'
+
+interface IInput extends React.InputHTMLAttributes<HTMLInputElement> {
+  register: UseFormRegister<FieldValues>
+  error: FieldError | Merge<FieldError, FieldErrorsImpl<any>> | undefined
+  id: string
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  label?: string
+  placeholder?: string
+}
+
+export function Input({
+  register,
+  error,
+  id,
+  onChange,
+  label,
+  placeholder,
+  ...inputProps
+}: IInput) {
+  return (
+    <>
+      {label && <label htmlFor={id}>{label}</label>}
+      <input
+        {...register(id, {
+          required: true,
+          onChange,
+        })}
+        id={id}
+        placeholder={placeholder}
+        {...inputProps}
+      />
+      {error && <span>Campo obrigatótio</span>}
+    </>
+  )
+}
