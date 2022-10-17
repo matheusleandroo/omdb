e37cbfe21a1be325732c196ed33dfeca2ee2dd22
Matheus Leandro <mattleandroo@outlook.com>
2022-10-17 14:17:38 -0300

    add scroll and some styles

--- src/pages/Listing/index.tsx
+++ src/pages/Listing/index.tsx
@@ -8,8 +8,8 @@ import { useFilters } from '../../hooks/Filters'
 
 import { MovieContent } from '../../components/MovieContent'
 
-import { ListingContainer } from './styles'
 import { Form } from '../../components/Form'
+import { ListingContainer, ListingContent } from './styles'
 
 export function Listing() {
   const { movies, updateMovies } = useMovies()
@@ -25,38 +25,47 @@ export function Listing() {
           totalPage: Math.ceil(data.data.totalResults / 10),
         })
       })
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      })
     },
     [filters, updateFilters, updateMovies],
   )
 
   useEffect(() => {
-    handleSubmit(filters.value, filters.page)
+    handleSubmit(filters.value, 1)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
     <ListingContainer>
-      <Form onSubmit={() => handleSubmit(filters.value, 1)} />
+      <Form
+        largeTitle={false}
+        onSubmit={() => handleSubmit(filters.value, 1)}
+      />
 
-      {movies && movies.length > 0 ? (
-        movies.map((movie) => {
-          return (
-            <div key={movie.imdbID}>
-              <MovieContent movie={movie} />
-            </div>
-          )
-        })
-      ) : (
-        <p>Nenhum resultado</p>
-      )}
+      <ListingContent>
+        {movies && movies.length > 0 ? (
+          movies.map((movie) => {
+            return (
+              <div key={movie.imdbID}>
+                <MovieContent movie={movie} />
+              </div>
+            )
+          })
+        ) : (
+          <h3>Nenhum resultado</h3>
+        )}
 
-      {movies && movies.length > 0 && (
-        <Paginate
-          page={filters.page}
-          pageCount={filters.totalPage}
-          onPageChange={(selected) => handleSubmit(filters.value, selected)}
-        />
-      )}
+        {movies && movies.length > 0 && (
+          <Paginate
+            page={filters.page}
+            pageCount={filters.totalPage}
+            onPageChange={(selected) => handleSubmit(filters.value, selected)}
+          />
+        )}
+      </ListingContent>
     </ListingContainer>
   )
 }
