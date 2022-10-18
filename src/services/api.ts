786776e586eaa06b4fc40d78837fb2e98935d361
Matheus Leandro <mattleandroo@outlook.com>
2022-10-18 12:35:45 -0300

    fix XMLHttpRequest cors

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 const omdbApi = import.meta.env.VITE_OMDB_API
 
 export const api = axios.create({
-  baseURL: 'http://www.omdbapi.com/',
+  baseURL: 'https://www.omdbapi.com/',
   params: {
     apikey: omdbApi,
   },
