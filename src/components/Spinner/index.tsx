7a2a267d877de4e1c5ed039d055ac572d02927ef
Matheus Leandro <mattleandroo@outlook.com>
2022-10-17 21:42:17 -0300

    adding spinner

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,16 @@
+import HashLoader from 'react-spinners/HashLoader'
+import { defaultTheme } from '../../styles/themes/default'
+
+import { SpinnerContainer } from './styles'
+
+interface ISpinner {
+  isLoading: boolean
+}
+
+export function Spinner({ isLoading }: ISpinner) {
+  return (
+    <SpinnerContainer>
+      <HashLoader loading={isLoading} color={defaultTheme.primary} />
+    </SpinnerContainer>
+  )
+}
