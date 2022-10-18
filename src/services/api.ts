338278ebbadd14eaf1f6c55ed00b3bc913eae91b
Matheus Leandro <mattleandroo@outlook.com>
2022-10-18 10:23:20 -0300

    adding env variable

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,10 @@
 import axios from 'axios'
 
+const omdbApi = import.meta.env.VITE_OMDB_API
+
 export const api = axios.create({
   baseURL: 'http://www.omdbapi.com/',
+  params: {
+    apikey: omdbApi,
+  },
 })
